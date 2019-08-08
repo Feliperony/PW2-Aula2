dd45956283cd893a59f514ef0f6ed7eac517d56e
Feliperony <ronyalvessobral@hotmail.com>
2019-08-07 21:58:33 -0300

    3 numeros if else

---
+++ dist/exercicio.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var result = (nota1 + nota2 + nota3) / 3;
+console.log("A média é " + result);
+if (result >= 7) {
+    console.log("Aprovado!!");
+}
+else {
+    console.log("Reprovado!!.");
+}
---
+++ exercicio.ts
@@ -0,0 +1,17 @@
+let nota1 : number = 10
+let nota2 : number = 9
+let nota3 : number = 5
+
+let result = (nota1 + nota2 + nota3) /3
+console.log("A média é " + result)
+
+if(result >= 7)
+{
+    console.log("Aprovado!!")
+}
+else
+{
+    console.log("Reprovado!!.")
+}
+
+
